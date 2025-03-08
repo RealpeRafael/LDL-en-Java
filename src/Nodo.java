6efffa4e59fe194947218221b0b3452ddda3c543
RealpeRafael <rafaelrealpe01@gmail.com>
2025-03-08 11:32:16 -0500

    Nodo.java

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,16 +1,15 @@
 package src;
 
+// Clase que representa un nodo en una lista doblemente ligada
 public class Nodo {
-    Empleado empleado;  // El objeto que contiene los datos
-    Nodo siguiente;     
-    Nodo anterior;      
+    Empleado empleado;  // Objeto que almacena los datos del empleado
+    Nodo siguiente;     // Referencia al siguiente nodo en la lista
+    Nodo anterior;      // Referencia al nodo anterior en la lista
 
-
+    // Constructor que inicializa un nodo con un empleado
     public Nodo(Empleado empleado) {
-        this.empleado = empleado;   
-        this.siguiente = null;
-        this.anterior = null;
+        this.empleado = empleado;   // Asigna el objeto empleado al nodo
+        this.siguiente = null;      // Inicialmente, el nodo no tiene siguiente
+        this.anterior = null;       // Inicialmente, el nodo no tiene anterior
     }
 }
-
-
